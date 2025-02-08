3adb27ae10e5c77d23b39a4350b8c49e2292fe3a
MOHAMED IBRAHIM <ibrahimchennai2005@gmail.com>
2025-02-08 10:53:56 +0530

    11am

--- DAY 6/Frequency_of_num.java
+++ DAY 6/Frequency_of_num.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Frequency_of_num {
     public static void main(String[] args) {
-        
+        int a[]={1,2,3,4,5,6,};
+        int freq[]=new int[a.length];
+        for (int i=0;i<a.length;i++){
+            int no=a[i];
+            int count=1;
+            for (int j=i+1;j<a.length;j++){
+                if(no==a[j]){
+                    count++;
+                    freq[j]=-1;
+                }
+            }
+            if(freq[i]!=-1){
+                freq[i]=count;
+            }
+        }
+        System.out.println(Arrays.toString(freq));
+        for(int i=0;i<=a.length;i++){
+            if(freq[i]!=-1){
+                // System.out.println(a[i]+" comes "+freq[i]+" times");
+            }
+        }
     }
 }
